2d9fdbadc5d34ba8fc4244c333e9805dc74d0964
danieljoucoski <daniel.joucoski@escola.pr.gov.br>
2026-09-18 12:01:11 -0300

    funcionando

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const userController = require("../controllers/userController");
+const router = express.Router();
+
+
+router.get('/users', userController.getAll);
+router.post('/users', userController.create);
+router.delete('/users/:id', userController.delete);
+
+module.exports = router;
